4cbf8f9d05135132f8e50dc59020f6eaa4df5451
YTSentralt <146125239+YTSentralt@users.noreply.github.com>
2023-09-26 21:09:55 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,18 @@
-function showMore() {
+let isContentVisible = false;
+
+function toggleContent() {
     const contentSection = document.getElementById('content');
-    contentSection.classList.add('show');
+    contentSection.classList.toggle('show', !isContentVisible);
 
     const shopSection = document.getElementById('shop-section');
     const contactSection = document.getElementById('contact-section');
 
-    shopSection.classList.add('show');
-    contactSection.classList.add('show');
+    shopSection.classList.toggle('show', isContentVisible);
+    contactSection.classList.toggle('show', isContentVisible);
+
+    const button = document.querySelector('.scroll-button button');
+    button.classList.toggle('clicked', isContentVisible);
+    isContentVisible = !isContentVisible;
 }
 
-function redirectToShop() {
-    // Implement your redirection to the shop here
-    console.log('Redirecting to the shop...');
-}
-
-function showContactInfo() {
-    const contactInfo = document.getElementById('contact-info');
-    contactInfo.style.display = 'block';
-}
-
-function contactViaEmail() {
-    // Implement your email contact functionality here
-    console.log('Contacting via email...');
-}
-
-function contactViaSMS() {
-    // Implement your SMS contact functionality here
-    console.log('Contacting via SMS...');
-}
+// Resten av JavaScript-funksjonene for shop, contact info osv. som tidligere
